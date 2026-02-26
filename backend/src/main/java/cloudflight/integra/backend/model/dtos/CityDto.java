3e90e94a9a7e6a991d6c65c4d10190e39739d932
CosovanuGabi912 <marius.cosovanu@stud.ubbcluj.ro>
2026-02-26 15:57:47 +0200

    #2: added UUID for id + validations
    Changed type for id from long to UUID, added DTO side validations + valid in CityController for post/put.

--- backend/src/main/java/cloudflight/integra/backend/model/dtos/CityDto.java
+++ backend/src/main/java/cloudflight/integra/backend/model/dtos/CityDto.java
@@ -2,17 +2,29 @@ package cloudflight.integra.backend.model.dtos;
 
 import cloudflight.integra.backend.model.CityStatus;
 import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CityDto {
-    private Long id;
-    private String name;
-    private String country;
-    private String description;
-    private Integer population;
-    private String imageUrl;
+
+    private UUID id;
     private CityStatus status;
+
+    @NotBlank(message = "City name is required")
+    private String name;
+    @NotBlank(message = "Country is required")
+    private String country;
+    @NotBlank(message = "Description is required")
+    private String description;
+    @NotNull(message = "Population is required")
+    private Integer population;
+    @NotBlank(message = "Image URL is required")
+    private String imageUrl;
+
+
 }
