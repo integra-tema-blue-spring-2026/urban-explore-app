8cb18d092761a4e6c6d8f2028679ce0c600e024b
Lupse <lupse.ioan.victor@gmail.com>
2026-03-16 14:03:10 +0200

    #12 : added follow logic, follow exceptions and PUT follow,PUT unfollow, GET followers, GET following

--- backend/src/main/java/cloudflight/integra/backend/service/UsersService.java
+++ backend/src/main/java/cloudflight/integra/backend/service/UsersService.java
@@ -1,5 +1,7 @@
 package cloudflight.integra.backend.service;
 
+import cloudflight.integra.backend.exceptions.custom.user.UserFollowException;
+import cloudflight.integra.backend.exceptions.custom.user.UserUnfollowException;
 import cloudflight.integra.backend.model.User;
 import cloudflight.integra.backend.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -32,5 +34,29 @@ public class UsersService{
     @Transactional
     public void delete(UUID id) {
         repository.deleteById(id);
+    }
+
+    @Transactional
+    public User followUser(UUID followerId, UUID targetUserId) {
+
+        User follower = repository.findById(followerId).orElseThrow(() -> new UserFollowException("Follower not found"));
+        User targetUser = repository.findById(targetUserId).orElseThrow(() -> new UserFollowException("Target User not found"));
+
+        follower.addFollower(targetUser);
+
+        repository.save(follower);
+        return repository.save(targetUser);
+    }
+
+    @Transactional
+    public User unfollowUser(UUID followerId, UUID targetUserId) {
+
+        User follower = repository.findById(followerId).orElseThrow(() -> new UserUnfollowException("Follower not found"));
+        User targetUser = repository.findById(targetUserId).orElseThrow(() -> new UserUnfollowException("Target User not found"));
+
+        follower.removeFollower(targetUser);
+
+        repository.save(follower);
+        return repository.save(targetUser);
     }
 }
