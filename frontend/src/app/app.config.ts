af0b0abbfcfb850fd95f612c30e3cb46a2979be0
AmaliaHaprian <amalia.haprian@stud.ubbcluj.ro>
2026-03-31 13:11:17 +0300

    #15: fixed app config duplication

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import {providePrimeNG} from 'primeng/config';
