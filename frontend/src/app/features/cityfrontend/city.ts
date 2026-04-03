4c6f232c497195ca48ecdc5f357d56c202de8c51
Cosovanu Marius-Gabriel <marius.cosovanu@stud.ubbcluj.ro>
2026-04-03 14:11:56 +0300

    #14: Frontend crud for city (#21)
    * #2: implement full City CRUD functionality
    
    Implemented the full City functionality ( without testing)
    
    * #2: Added a test file and CityException
    
    Test + CityException + fixed double api bug + dependencies in buil.gradle
    
    * #2: added UUID for id + validations
    
    Changed type for id from long to UUID, added DTO side validations + valid in CityController for post/put.
    
    * #2 Added global exception handler
    
    global exception handler and small rename for city exception
    
    * #2:add ErrorResponse entity
    
    * #2:add CreateCityDto and UpdateCityDto, changed logic in service and mapper for update/create operation
    
    * #2:Eliminated some useless imports
    
    * #2:Used the global exception already created
    
    * #2:Used the global exception already created
    
    * #14: Added front-end city
    
    * #14: Deleted the copy of some files and finished frontend crud
    
    * #11: added POI and city filtering (#19)
    
    * #11: fixed code style
    
    * #2: implement full City CRUD functionality
    
    Implemented the full City functionality ( without testing)
    
    * #2: Added a test file and CityException
    
    Test + CityException + fixed double api bug + dependencies in buil.gradle
    
    * #2: added UUID for id + validations
    
    Changed type for id from long to UUID, added DTO side validations + valid in CityController for post/put.
    
    * #2 Added global exception handler
    
    global exception handler and small rename for city exception
    
    * #2:add ErrorResponse entity
    
    * #2:add CreateCityDto and UpdateCityDto, changed logic in service and mapper for update/create operation
    
    * #2:Eliminated some useless imports
    
    * #2:Used the global exception already created
    
    * #2:Used the global exception already created
    
    * #14: Added front-end city
    
    * #14: Deleted the copy of some files and finished frontend crud
    
    * #14: Deleted the copy of some files and finished frontend crud
    
    * #14:Rebased
    
    * Combined services and city models
    
    * Combined services and city models
    
    * #14: Data from frontend matches the backend data
    
    Made non null form and created matching frontend models for the backend dtos
    
    * Combined services and city models
    
    * Combined services and city models
    
    * Combined services and city models
    
    ---------
    
    Co-authored-by: Acatrinei Robert-Marian <92254454+RobertAcatrinei123@users.noreply.github.com>

---
+++ frontend/src/app/features/cityfrontend/city.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CityService } from '../../core/api/services/city.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {City, CityStatus, CreateCityRequest, UpdateCityRequest} from '../../shared/models/city.model';
+
+
+@Component({
+  selector: 'app-city',
+  templateUrl: './city.html',
+  styleUrl: './city.css',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule]
+})
+export class CityComponent implements    OnInit {
+  cities: City[] = [];
+  editingId: string | null = null;
+
+  cityForm = new FormGroup({
+    name: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    country: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    description: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    population: new FormControl(1, { nonNullable: true, validators: [Validators.required, Validators.min(1)] }),
+    imageUrl: new FormControl('', { nonNullable: true, validators: [Validators.required] })
+  });
+
+  constructor(private cityService: CityService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void { this.loadCities(); }
+
+  loadCities() {
+    this.cityService.getCities().subscribe((data) => {
+      this.cities = data;
+      this.cdr.markForCheck();
+    });
+  }
+
+  startEdit(c: City) {
+    this.editingId = c.id!;
+    this.cityForm.patchValue(c);
+    this.cityForm.get('name')?.disable();
+    this.cityForm.get('country')?.disable();
+    this.cityForm.get('population')?.disable();
+  }
+
+  private enableAllFields() {
+    this.cityForm.get('name')?.enable();
+    this.cityForm.get('country')?.enable();
+    this.cityForm.get('population')?.enable();
+  }
+
+  saveCity() {
+    if (!this.cityForm.valid) return;
+    const rawValue = this.cityForm.getRawValue();
+
+    if (this.editingId) {
+      const updateData: UpdateCityRequest = {
+        description: rawValue.description,
+        imageUrl: rawValue.imageUrl
+      };
+      this.cityService.updateCity(this.editingId, updateData).subscribe(() => {
+        alert('City updated successfully!');
+        this.finalizeAction();
+      });
+    } else {
+      const createData: CreateCityRequest = {
+        name: rawValue.name,
+        country: rawValue.country,
+        description: rawValue.description,
+        population: rawValue.population,
+        imageUrl: rawValue.imageUrl
+      };
+      this.cityService.addCity(createData).subscribe(() => {
+        alert('City added successfully!');
+        this.finalizeAction();
+      });
+    }
+  }
+
+  onDelete(id: string | undefined) {
+    if (!id || !confirm('Please confirm you want to delete this')) return;
+    this.cityService.deleteCity(id).subscribe(() => this.loadCities());
+  }
+
+  cancelEdit() {
+    this.editingId = null;
+    this.cityForm.reset({ population: 1 });
+    this.enableAllFields();
+  }
+  private finalizeAction() {
+    this.editingId = null;
+    this.enableAllFields(); // Unlock
+    this.loadCities();
+    this.cityForm.reset({ population: 1 });
+  }
+
+  protected readonly CityStatus = CityStatus;
+}
